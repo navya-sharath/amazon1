3feb48a6cf0e151824458b2fa565914d0cbd3199
navya-sharath <Navyakrishnasharath@gmail.com>
2024-11-13 11:56:51 +0530

    navya

---
+++ frist_class/src/main/java/frist_class/Public.java
@@ -0,0 +1,13 @@
+package frist_class;
+
+public class Public {
+	public static void main(String[] args) {
+		String name="navya";
+		int a=10,b=30,c=a+b;
+		
+		System.out.println(name);
+		System.out.println(c);
+		
+	}
+
+}
