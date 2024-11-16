b2d57869e58d8fd0dcd6b441bc85da7822b79d19
navya-sharath <Navyakrishnasharath@gmail.com>
2024-11-16 16:39:35 +0530

    nested

---
+++ variabls/src/amazon_project/Nested.java
@@ -0,0 +1,30 @@
+package amazon_project;
+
+public class Nested {
+	public static void main(String[] args) {
+		String student="navya";
+		int marks=29;
+		
+		if(marks>0&& marks<=30) {
+			System.out.println("Navya is Fail");
+		}
+			
+		else if (marks>31 && marks<=50) {
+			System.out.println("Navya grade is C");
+		} 
+		else if (marks>51 && marks<=80) {
+			System.out.println("Navya grade is B");
+			
+		}
+		else if (marks>81 && marks<=100) {
+			System.out.println("Navya grade is A");
+			
+		}
+		else {
+			System.out.println("wants more improvement");
+		}
+		
+	}
+	}
+
+
