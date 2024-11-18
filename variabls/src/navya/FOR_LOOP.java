5753282547f061c28cbde2df2762fcccf153f4cf
navya-sharath <Navyakrishnasharath@gmail.com>
2024-11-18 10:52:02 +0530

    for loop

---
+++ variabls/src/navya/FOR_LOOP.java
@@ -0,0 +1,19 @@
+package navya;
+
+public class FOR_LOOP {
+public static void main(String[] args) {
+	//int num=20;
+	System.out.println("1 to 20 even number");
+	
+	for(int i=0;i<20;i+=2) {
+	
+		
+		if (i%2==0) {
+	
+		System.out.println(i);
+		
+	}
+}
+}
+}
+
---
+++ variabls/src/navya/LOOP.java
@@ -0,0 +1,17 @@
+package navya;
+
+public class LOOP {
+	public static void main(String[] args) {
+		char[] letter ={'N','A','V','Y','A'};
+		//String name="navya";
+		String st=new String 
+		
+		for(char leter:leters) {
+			if(string.)
+				
+			}
+			
+		}
+	}
+
+
