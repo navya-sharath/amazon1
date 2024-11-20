374e007103f8dac6c7b618955ae63e4a16747032
navya-sharath <Navyakrishnasharath@gmail.com>
2024-11-20 19:49:30 +0530

    parameterized and non parameterized(static and non static)

--- variabls/src/navya/LOOP.java
+++ variabls/src/navya/LOOP.java
@@ -1,14 +1,23 @@
 package navya;
 
+
 public class LOOP {
-	
-	
-	public static void main(String[] args) {
-		int[] numbers= {-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10};
-		for(int number:numbers) {
-			System.out.println(number);
-			
-		}
+  public static void static_parameter(String name) {
+	  System.out.println(name);
+  }
+	  
+	  
+	  
+	  
+	  public static void main(String[] args) {
+		  static_parameter.
+	  
+	 
+	 
+	  
+	  
+	  
+  
 	 
 			
 		
---
+++ variabls/src/navya/Parameterised.java
@@ -0,0 +1,40 @@
+package navya;
+
+public class Parameterised {
+	public static void static_parameter(int a,int b) {
+		System.out.println(a+b);
+	}
+		
+		public static void non_parameter(int x,int y) {
+			
+		
+         System.out.println(x-y);
+		
+		
+	}
+		public void nonstatic_nonparameterized() {
+			int s=45;
+			int p=67;
+			System.out.println(s*p);
+			
+			
+		
+		}
+		public void nonstatic_paramereter(int m,int n){
+			System.out.println(m-n);
+			
+		}
+		
+		
+	public static void main(String[] args) {
+		Parameterised.static_parameter(5, 6);
+		Parameterised.static_parameter(8, 9);
+		Parameterised pt = new Parameterised();
+		pt.nonstatic_nonparameterized();
+		Parameterised pa=new Parameterised();
+		pa.nonstatic_paramereter(10, 50);
+		
+		
+	}
+
+}
