1c27e1f00fa5f240f980a3b74fb1aaad7937db4b
navya-sharath <Navyakrishnasharath@gmail.com>
2024-11-20 15:17:56 +0530

    positive and negative(for each loop)

--- variabls/src/navya/LOOP.java
+++ variabls/src/navya/LOOP.java
@@ -1,17 +1,20 @@
 package navya;
 
 public class LOOP {
+	
+	
 	public static void main(String[] args) {
-		char[] letter ={'N','A','V','Y','A'};
-		//String name="navya";
-		String st=new String 
-		
-		for(char leter:leters) {
-			if(string.)
-				
-			}
+		int[] numbers= {-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10};
+		for(int number:numbers) {
+			System.out.println(number);
 			
 		}
+	 
+			
+		
+			
+		
+	
+
 	}
-
-
+}
